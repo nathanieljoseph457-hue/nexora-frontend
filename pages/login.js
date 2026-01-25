5473daa2be7f027a348a6f8bf1feec38a6030ed9
nathanieljoseph457-hue <nathanieljoseph457@gmail.com>
2026-01-25 23:28:21 +0100

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ padding: "40px", color: "#fff" }}>
+      <h1>Login</h1>
+      <p>Login to your Nexora Capital account.</p>
+    </div>
+  );
+}
