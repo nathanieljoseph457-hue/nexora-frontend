d396bf0864a57e7836781ad05231d8f66c8b9c93
nathanieljoseph457-hue <nathanieljoseph457@gmail.com>
2026-01-25 22:43:22 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div style={{ padding: "40px", color: "#fff" }}>
+      <h1>About Nexora Capital</h1>
+      <p>
+        Nexora Capital is a crypto investment platform built to help users
+        grow and manage digital assets securely.
+      </p>
+    </div>
+  );
+}
