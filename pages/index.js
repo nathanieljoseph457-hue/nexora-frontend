e35ed5d204aea7c375f18615ffff0ee2651b8d30
nathanieljoseph457-hue <nathanieljoseph457@gmail.com>
2026-01-25 20:11:05 +0100

    Add Home component for Nexora Capital landing page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Nexora Capital</h1>
+      <p>Crypto Investment Platform</p>
+    </div>
+  );
+}
